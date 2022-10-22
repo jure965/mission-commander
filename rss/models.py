acc179cdec5e46e09f05d29bc3406078f0f429ad
Jure Vengušt <jure@equaleyes.com>
2022-10-22 15:51:31 +0200

    add TransmissionClient model

--- rss/models.py
+++ rss/models.py
@@ -12,3 +12,12 @@ class Torrent(models.Model):
 class Feed(models.Model):
     url = models.URLField()
     expire = models.DateTimeField()
+    download_dir = models.CharField(max_length=2048, blank=True)
+
+
+class TransmissionClient(models.Model):
+    host = models.CharField(max_length=2048)
+    port = models.CharField(max_length=10)
+    username = models.CharField(max_length=2048)
+    password = models.CharField(max_length=2048)
+    feeds = models.ManyToManyField(Feed, related_name="transmission_clients")
