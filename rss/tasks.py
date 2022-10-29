398ef05458aa2971171c3b017986b513034fb2bc
Jure Vengušt <jure@equaleyes.com>
2022-10-29 21:20:02 +0200

    improve readability and order reliability

--- rss/tasks.py
+++ rss/tasks.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from time import mktime
+from typing import List
+
 import pytz
 
 import feedparser
@@ -39,35 +41,41 @@ def parse_feed(feed_id: int):
     if feed.chronological:
         d.entries.sort(key=lambda x: x.pub)
 
+    if feed.regex_filter:
+        d.entries = [e for e in d.entries if feed.regex_filter.search(e.title)]
+
+    if feed.ignore_older_than:
+        d.entries = [e for e in d.entries if e.pub > feed.ignore_older_than]
+
+    if feed.ignore_newer_than:
+        d.entries = [e for e in d.entries if e.pub < feed.ignore_newer_than]
+
+    torrents = []
+
     for entry in d.entries:
-        if feed.regex_filter and not feed.regex_filter.search(entry.title):
-            continue  # skip if regex_filter is set and no match found in entry title
-
-        if feed.ignore_older_than and entry.pub < feed.ignore_older_than:
-            continue
-
-        if feed.ignore_newer_than and entry.pub > feed.ignore_newer_than:
-            continue
-
-        torrent, created = Torrent.objects.get_or_create(
-            title=entry.title,
-            link=entry.link,
-            published=entry.pub,
+        torrents.append(
+            Torrent.objects.get_or_create(
+                title=entry.title,
+                link=entry.link,
+                published=entry.pub,
+            )
         )
 
-        if created:
-            for client in feed.transmission_clients.all():
-                add_torrent.delay(
-                    torrent_id=torrent.id,
-                    client_id=client.id,
-                    download_dir=feed.download_dir,
-                    start_paused=feed.start_paused,
-                )
+    torrent_ids = [t[0].id for t in torrents if t[1]]
+
+    for client in feed.transmission_clients.all():
+        send_torrents.delay(
+            torrent_ids=torrent_ids,
+            client_id=client.id,
+            download_dir=feed.download_dir,
+            start_paused=feed.start_paused,
+        )
 
 
 @app.task
-def add_torrent(torrent_id: int, client_id: int, download_dir: str, start_paused: bool):
-    torrent = Torrent.objects.get(id=torrent_id)
+def send_torrents(
+    torrent_ids: List, client_id: int, download_dir: str, start_paused: bool
+):
     tc = TransmissionClient.objects.get(id=client_id)
 
     rpc_client = Client(
@@ -79,4 +87,8 @@ def add_torrent(torrent_id: int, client_id: int, download_dir: str, start_paused
         path=tc.rpc_path,
     )
 
-    rpc_client.add_torrent(torrent.link, download_dir=download_dir, paused=start_paused)
+    for torrent_id in torrent_ids:
+        torrent = Torrent.objects.get(id=torrent_id)
+        rpc_client.add_torrent(
+            torrent.link, download_dir=download_dir or None, paused=start_paused
+        )
