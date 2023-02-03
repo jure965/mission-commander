074b5e20cef576f2c3976bd12941b8edf9d8e5dd
Jure <jure.vengust@gmail.com>
2023-02-03 20:16:15 +0100

    chore: optimize login form

--- rss/views/auth.py
+++ rss/views/auth.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import (
     LoginView as BuiltinLoginView,
     LogoutView as BuiltinLogoutView,
@@ -7,6 +8,8 @@ from django.urls import reverse_lazy
 
 class LoginView(BuiltinLoginView):
     template_name = "rss/login.html"
+    form_class = AuthenticationForm
+    success_url = reverse_lazy("feed-list")
 
 
 class LogoutView(BuiltinLogoutView):
