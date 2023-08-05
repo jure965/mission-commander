02d69519d25ab7996282d86be8d843fecfee8524
Jure <jure.vengust@gmail.com>
2023-08-06 00:10:24 +0200

    chore: replace --remove-untracked with --sync

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,7 @@
 
 systemctl stop mc-web.service mc-beat.service mc-worker.service
 
-/root/.local/bin/poetry install --remove-untracked --no-root --without dev
+/root/.local/bin/poetry install --sync --no-root --without dev
 /root/.local/bin/poetry run python manage.py migrate
 /root/.local/bin/poetry run python manage.py collectstatic --noinput
 
