c2126bf064043e3822ccabe2cdbea1f15bb49a01
Jure Vengušt <jure.vengust@equaleyes.com>
2026-09-19 18:42:55 +0200

    feat: add client_type field to TransmissionClient model

--- rss/models/transmission_client.py
+++ rss/models/transmission_client.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class TransmissionClient(models.Model):
+    class ClientType(models.TextChoices):
+        TRANSMISSION = "transmission", "transmission"
+        QBITTORRENT = "qbittorrent", "qbittorrent"
+
     class Protocol(models.TextChoices):
         HTTP = "http", "http"
         HTTPS = "https", "https"
@@ -15,6 +19,9 @@ class TransmissionClient(models.Model):
     username = models.CharField(max_length=2048, blank=True)
     password = models.CharField(max_length=2048, blank=True)
     rpc_path = models.CharField(max_length=2048, default="/transmission/")
+    client_type = models.CharField(
+        max_length=50, choices=ClientType.choices, default=ClientType.TRANSMISSION
+    )
 
     def __str__(self):
         return f"{self.name} ({self.host}:{self.port})"  # noqa: E231
